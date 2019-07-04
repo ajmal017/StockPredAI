e9ce849aeb68b8bd0a72001e97ccf5659e00e2b1
Rafael Ruiz <ramruizni@gmail.com>
2019-07-04 18:25:22 -0500

    Brought code back that I deleted by mistake

---
+++ Python Backend/constants.py
@@ -0,0 +1,16 @@
+STOCKS_LOCATION = "datasets/stock/"
+FOREX_LOCATION = "datasets/forex/"
+INDEX_LOCATION = "datasets/index/"
+
+BASE_URL = "https://s3.us-east-2.amazonaws.com/frd001/"
+
+MAP_ENDPOINTS = {
+    "AMZN": "AMZN_1MIN_ze7avh",
+    "APPL": "AAPL_1MIN_ct7eed",
+    "INTC": "INTC_1MIN_9epm7z",
+    "JPM": "JPM_1MIN_e2dh44",
+    "BAC": "BAC_1MIN_hbth0j"
+}
+
+EUR_USD_ENDPOINT = "EURUSD_5MIN_99rasp"
+SPX_ENDPOINT = "SPX_1MIN_8v96zt"
