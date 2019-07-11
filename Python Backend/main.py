48e6fae4a3bef6bcc9dca1744fa7f71d3e34a9c8
Rafael Ruiz <ramruizni@gmail.com>
2019-07-11 12:01:59 -0500

    Obviating repetitive steps like downloading datasets again

--- Python Backend/main.py
+++ Python Backend/main.py
@@ -4,17 +4,17 @@ from network.push_firestore import push_stocks_data
 pd.set_option('display.max_columns', None)
 pd.options.mode.chained_assignment = None
 
-STOCK_SYMBOLS = ["AMZN", "INTC"]
+STOCK_SYMBOLS = ["JPM", "BAC"]
 TIME_STEPS = 10  # minutes to consider
 EPOCHS = 10
 BATCH_SIZE = 1024
 FAST_FOURIER_TIMEOUT_SECONDS = 20
 
-download_starting_data(STOCK_SYMBOLS)
+#download_starting_data(STOCK_SYMBOLS)
 
-stocks = preprocess_data(STOCK_SYMBOLS, FAST_FOURIER_TIMEOUT_SECONDS)
+#stocks = preprocess_data(STOCK_SYMBOLS, FAST_FOURIER_TIMEOUT_SECONDS)
 
-train_data(STOCK_SYMBOLS, stocks, TIME_STEPS, EPOCHS, BATCH_SIZE)
+#train_data(STOCK_SYMBOLS, stocks, TIME_STEPS, EPOCHS, BATCH_SIZE)
 
 in_stocks, push_stocks = fetch_new_data(STOCK_SYMBOLS, TIME_STEPS)
 
